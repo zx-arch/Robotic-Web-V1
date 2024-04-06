c318d40364165038a227e872f8820c2175ebb47c
renaldikristian <renaldi.kristian@ti.ukdw.ac.id>
2024-04-06 11:02:56 +0700

    Menambahkan Middleware Auto Create CSRF Token

---
+++ app/Http/Middleware/AutoCreateCsrfToken.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class AutoCreateCsrfToken extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = parent::handle($request, $next);
+
+        // Periksa jika ada upaya untuk menghapus token CSRF dari sesi
+        if ($request->session()->has('_token') && $request->session()->isStarted() && $request->session()->token() !== $request->input('_token')) {
+            // Jika ada upaya penghapusan token CSRF, set kembali token CSRF ke nilai awal
+            $request->session()->put('_token', $request->session()->token());
+        }
+
+        return $response;
+    }
+}
